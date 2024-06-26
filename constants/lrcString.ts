21a1e00ca35ca0f133af2fb92d49fe990da04a5f
Akshay Jadhav <akshayjadhav461999@gmail.com>
2024-06-26 20:28:39 +0530

    Updated lrc data to include line for music without lyrics

--- constants/lrcString.ts
+++ constants/lrcString.ts
@@ -5,4 +5,5 @@ export const lrcString = `[ti: Lorem ipsum]
 [00:00.00] <00:00.04> Lorem <00:00.16> ipsum <00:00.82> dolor <00:01.29> sit <00:01.63> amet, <00:03.09> consectetur <00:03.37> adipiscing <00:05.92> elit. 
 [00:06.47] <00:07.67> Sed <00:07.94> do <00:08.36> eiusmod <00:08.63> tempor <00:10.28> incididunt <00:10.53> ut <00:13.09> labore 
 [00:13.34] <00:14.32> et <00:14.73> dolore <00:15.14> magna <00:15.57> aliqua. <00:16.09> Ut <00:16.46> enim
-[00:17.00] <00:17.04> ad <00:17.16> minim <00:17.82> veniam, <00:18.29> quis <00:18.63> nostrud <00:19.09> exercitation`;
+[00:17.44] 
+[00:23.00] <00:24.40> orci. <00:24.70> Mauris <00:25.14> eu <00:25.30> dui`;
